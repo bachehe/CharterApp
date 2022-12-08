cb54baa9979c920978eebbcd2d39ed21c13d4df7
Damian Bachowski <damianbacho9@gmail.com>
2022-12-08 17:35:03 +0100

    feat: add chart with hard coded data

--- CharterApp/ViewModels/GraphViewModel.cs
+++ CharterApp/ViewModels/GraphViewModel.cs
@@ -4,18 +4,40 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Media;
+using ScottPlot;
+using ScottPlot.WPF;
 
 namespace CharterApp.ViewModels
 {
     public class GraphViewModel
     {
-        public PointCollection Points { get; }
+        public ScottPlot.WpfPlot DataPlot { get; set; } = new ScottPlot.WpfPlot();
+        //public PointCollection Points { get; }
         public GraphViewModel()
         {
-            Points = new();
-            Points.Add(new(50, 100));
-            Points.Add(new(70, 250));
-            Points.Add(new(230, 299));
+            //Points = new();
+            //Points.Add(new(50, 100));
+            //Points.Add(new(70, 250));
+            //Points.Add(new(230, 299));
+
+            double[] dataX = new double[] { 1, 2, 3, 4, 5 };
+            double[] dataY = new double[] { 1, 4, 9, 16, 25 };
+
+            DataPlot.BorderBrush = new SolidColorBrush(Colors.White);
+            DataPlot.Plot.AddScatter(dataX, dataY, label:"first");
+
+            double[] dataXX = new double[] { 5, 7, 12, 16, 25 };
+            double[] dataYY = new double[] { 1, 9, 17, 21, 43 };
+            DataPlot.Plot.AddScatter(dataXX, dataYY, label: "second");
+            DataPlot.Plot.Legend();
+            DataPlot.Plot.Title("CHART");
+            DataPlot.Plot.XLabel("Angle");
+            DataPlot.Plot.YLabel("EGW");
+            DataPlot.Plot.Layout(left: 100, right: 100, bottom: 100, top: 50);
+            DataPlot.Refresh();
+            
+            
+
         }
     }
 }

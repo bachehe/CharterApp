6f6dfa53dcb523a6844c10cc0ff0919a566a7451
GeorgeKarlinzer <georg11.01@mail.ru>
2022-12-20 16:58:16 +0100

    feat: add linear factor view model

---
+++ CharterApp/Calculate/Data.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+using System.IO;
+
+namespace CharterApp.Calculate
+{
+    public static class Data
+    {
+        public static List<double> Densities { get; } 
+        public static List<double> MoValues { get; }
+        public static List<double> CuValues { get; }
+        public static List<double> CoValues { get; }
+        public static List<double> CrValues { get; }
+
+        static Data()
+        {
+            Densities = new();
+            MoValues = new();
+            CuValues = new();
+            CoValues = new();
+            CrValues = new();
+
+            var filePath = "./Data/basecsv.csv";
+
+            foreach (var line in File.ReadAllLines(filePath))
+            {
+                var values = line.Split(';');
+
+                var density = double.Parse(values[0]);
+                var mo = double.Parse(values[1]);
+                var cu = double.Parse(values[2]);
+                var co = double.Parse(values[3]);
+                var cr = double.Parse(values[4]);
+
+                Densities.Add(density);
+                MoValues.Add(mo);
+                CuValues.Add(cu);
+                CoValues.Add(co);
+                CrValues.Add(cr);
+            }
+        }
+    }
+}
---
+++ CharterApp/ViewModels/LinearFactorCalculatingViewModel.cs
@@ -0,0 +1,41 @@
+﻿using CharterApp.Calculate;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CharterApp.ViewModels
+{
+    public class LinearFactorCalculatingViewModel : INotifyPropertyChanged
+    {
+        public double Density { get; set; }
+
+        public double MoPercentage { get; set; }
+        public double CuPercentage { get; set; }
+        public double CoPercentage { get; set; }
+        public double CrPercentage { get; set; }
+
+        public double LinearFactor { get; set; }
+        
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+
+        private void Calculate()
+        {
+            if (MoPercentage + CuPercentage + CoPercentage + CrPercentage != 1)
+                return;
+
+            var index = Data.Densities.IndexOf(Density);
+
+            LinearFactor = (Data.MoValues[index] * MoPercentage +
+                Data.CuValues[index] * CuPercentage +
+                Data.CoValues[index] * CoPercentage +
+                Data.CrValues[index] * CrPercentage) * Density;
+
+            PropertyChanged?.Invoke(this, new(nameof(LinearFactor)));
+        }
+    }
+}
